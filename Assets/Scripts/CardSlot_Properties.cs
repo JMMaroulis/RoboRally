c8c51cf38f2364e273b99cb811556bf86a7903ce
unknown <jamesmaroulis@gmail.com>
2019-10-01 18:33:25 +0100

    Basic processing of cards in cardslots

---
+++ Assets/Scripts/CardSlot_Properties.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSlot_Properties : MonoBehaviour
+{
+
+    public int player;
+    public int slotNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ExecuteCards.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExecuteCards : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void beepbeep()
+    {
+        Debug.Log("beepbeep");
+
+        //get cardslots
+        GameObject[] cardSlots = GameObject.FindGameObjectsWithTag("CardSlot");
+
+        foreach (GameObject cardSlot in cardSlots)
+        {
+            GameObject card = cardSlot.transform.GetChild(0).gameObject;
+            CardDisplay cardDisplay = card.GetComponent<CardDisplay>();
+            Debug.Log(cardDisplay.action.text);
+            Debug.Log(cardSlot.GetComponent<CardSlot_Properties>().slotNumber);
+        }
+
+    }
+}
